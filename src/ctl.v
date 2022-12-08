162e90364af4d585e424d0e232c2c9a0dd57bc82
abdtyx <1507844577@qq.com>
2022-12-08 22:33:50 +0800

    ctl complete

--- src/ctl.v
+++ src/ctl.v
@@ -6,11 +6,85 @@ module mcu (
     output wire RegWr,
     output wire Branch,
     output wire MemtoReg,
-    output wire ALUOp,
+    output wire[`ALUOP_LEN-1:0] ALUOp,
     output wire MemWr,
     output wire MemRd,
     output wire ALUSrc
 );
+
+    reg[`ALUOP_LEN-1:0] aluop;
+    reg regdst, jump, regwr, branch, memtoreg, memwr, memrd, alusrc;
+
+    always @(OpCode) begin
+        case (OpCode)
+            `R: begin
+                regdst = 1;
+                regwr = 1;
+                alusrc = 0;
+                memrd = 0;
+                memwr = 0;
+                memtoreg = 0;
+                branch = 0;
+                jump = 0;
+                aluop = `ALUOP_LEN'b10;
+            end
+            `LW: begin
+                regdst = 0;
+                regwr = 1;
+                alusrc = 1;
+                memrd = 1;
+                memwr = 0;
+                memtoreg = 1;
+                branch = 0;
+                jump = 0;
+                aluop = `ALUOP_LEN'b00;
+            end
+            `SW: begin
+                regdst = 0;
+                regwr = 0;
+                alusrc = 1;
+                memrd = 0;
+                memwr = 1;
+                memtoreg = 0;
+                branch = 0;
+                jump = 0;
+                aluop = `ALUOP_LEN'b00;
+            end
+            `BEQ: begin
+                regdst = 0;
+                regwr = 0;
+                alusrc = 0;
+                memrd = 0;
+                memwr = 0;
+                memtoreg = 0;
+                branch = 1;
+                jump = 0;
+                aluop = `ALUOP_LEN'b01;
+            end
+            `J: begin
+                regdst = 0;
+                regwr = 0;
+                alusrc = 0;
+                memrd = 0;
+                memwr = 0;
+                memtoreg = 0;
+                branch = 0;
+                jump = 1;
+                aluop = `ALUOP_LEN'b00;
+            end
+        endcase
+    end
+
+    assign RegDst = regdst;
+    assign Jump = jump;
+    assign RegWr = regwr;
+    assign Branch = branch;
+    assign MemtoReg = memtoreg;
+    assign ALUOp = aluop;
+    assign MemWr = memwr;
+    assign MemRd = memrd;
+    assign ALUSrc = alusrc;
+
 endmodule
 
 module alucu (
@@ -27,7 +101,7 @@ module alucu (
                 sig = `ADD;
             `ALUOP_LEN'b01:
                 sig = `SUB;
-            `ALUOP_LEN'b1x:
+            `ALUOP_LEN'b1x: begin
                 case (Funct)
                     `FUNCT_LEN'b100000:
                         sig = `ADD;
@@ -42,6 +116,7 @@ module alucu (
                     `FUNCT_LEN'b101010:
                         sig = `SLT;
                 endcase
+            end
         endcase
     end
 
